0427e2e07ad95e45835d301df35239ab3b8040bb
K1 <searchfordeveloping@gmail.com>
2022-10-16 15:49:05 -0700

    Add project files.

---
+++ Singleton/Implementation.cs
@@ -0,0 +1,21 @@
+﻿namespace Singleton
+{
+    public class Logger
+    {
+        //thread safe, it handles locking 
+        private static readonly Lazy<Logger> _lazyLogger = new Lazy<Logger>(() => new Logger());
+
+        /// <summary>
+        /// old and not thread safe way
+        /// </summary>
+        //private static Logger? _instance;
+        //public static Logger Instance => _instance ??= new Logger();
+
+        public static Logger Instance => _lazyLogger.Value;
+
+        protected Logger()
+        {
+        }
+        public static void Log(string message) => Console.WriteLine($"Message to log {message}");
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿using Singleton;
+
+Console.Title = "Singleton";
+
+var instance1 = Logger.Instance;
+var instance2 = Logger.Instance;
+
+if(instance1 == instance2 && instance2 == Logger.Instance)
+{
+    Console.WriteLine("Instances are the same");
+}
+
+Logger.Log($"Message from {nameof(instance1)}");
+Logger.Log($"Message from {nameof(instance2)}");
+Logger.Log($"Message from {nameof(Logger.Instance)}");
